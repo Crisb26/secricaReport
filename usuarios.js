92c9d930c510e76ef2c0da0b28410ef2dfb7c7dd
Cristopher Bueno <buenocris02@gmail.com>
2025-07-01 11:23:39 -0500

    se elimina usuarios.json y se reemplaza por usuarios.js para poder permitir imports directos en el entorno frontend sin el fetch, mejorando asi la organizacion y la reutilizacion de los usuarios base

---
+++ usuarios.js
@@ -0,0 +1,21 @@
+export const usuarios = [
+  {
+    id: 1,
+    username: "cris",
+    email: "buenocris02@gmail.com",
+    password: "2hg1ek-ylsge0", //contraseña cifrada desde consola
+    fullName: "Cristopher Bueno",
+    rol: "admin",
+    activo: true
+  },
+  {
+    id: 2,
+    username: "sebas",
+    email: "sebas@gmail.com",
+    password: "2xh7aq-4so4hp", //contraseña cifrada
+    fullName: "Sebastian Pinilla",
+    rol: "funcionario",
+    activo: true
+  }
+];
+
